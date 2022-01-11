c7c1c0b8fade62b26cf89d999188922d8dd91cf5
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-11 20:11:22 +0100

    created new view and stylesheet; passed data from new controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,15 @@
 # Games controller
 class GamesController < ApplicationController
-  def new; end
+  def new
+    @letters = []
+    alpbt = ('a'..'z').to_a
+
+    until @letters.length == 10
+      ltr = alpbt.sample
+      @letters << ltr unless @letters.include?(ltr)
+    end
+    @letters
+  end
 
   def score; end
 end
